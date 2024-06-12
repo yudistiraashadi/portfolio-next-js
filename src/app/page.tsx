cdb76af1afcac4b0464866250d9af46e1ba0a627
Yudistira Ashadi <distra96@yahoo.com>
2024-06-12 18:33:32 +0700

    add spacing for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { Highlight, HeroHighlight } from "@/components/ui/hero-highlight";
+import { Meteors } from "@/components/ui/meteors";
 import { Container } from "@/components/container";
 
 import photoYudis from "@/assets/images/photo_yudis.jpg";
@@ -13,27 +14,29 @@ export default function Home() {
         <div>
           <HeroHighlight>
             <Container>
-              <div className="grid h-full min-h-[40rem] gap-4 py-4 lg:grid-cols-5">
+              <div className="relative grid h-full min-h-[40rem] gap-4 overflow-clip py-4 lg:grid-cols-5">
+                <Meteors number={10} />
+
                 <div className="flex flex-col justify-center lg:col-span-3">
-                  <h2 className="text-2xl font-bold leading-none">
+                  <h2 className="text-xl font-bold leading-none lg:text-2xl">
                     Hello &#x1F44B;,
                   </h2>
-                  <h2 className="mb-12 text-2xl font-bold">
+                  <h2 className="mb-12 text-xl font-bold lg:text-2xl">
                     My name is Yudistira Ashadi
                   </h2>
 
-                  <h1 className="mb-16 text-5xl font-bold">
+                  <h1 className="mb-16 text-3xl font-bold lg:text-5xl">
                     I'm a{" "}
                     <Highlight>
-                      <span className="px-4">React Developer</span>
+                      <span className="lg:px-4">React Developer</span>
                     </Highlight>
                   </h1>
 
-                  <h2 className="mb-4 text-2xl font-semibold">
+                  <h2 className="mb-4 text-xl font-semibold lg:text-2xl">
                     Specializing in Next.JS, React, and React Native
                   </h2>
 
-                  <p>
+                  <p className="text-sm lg:text-base">
                     I'm <span className="font-bold">Yudistira Ashadi</span> a
                     software engineer based in Indonesia. I've been creating
                     hybrid mobile apps and website using React Native, React,
